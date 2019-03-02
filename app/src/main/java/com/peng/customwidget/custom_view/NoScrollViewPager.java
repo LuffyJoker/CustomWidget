5ff5f9ded0e77e1a3dbf0c42ae0d2ed5081e538a
pengqi <m18380449235_1@163.com>
2019-03-02 14:42:27 +0800

    1、新增不能滑动的 ViewPager

---
+++ app/src/main/java/com/peng/customwidget/custom_view/NoScrollViewPager.java
@@ -0,0 +1,32 @@
+package com.peng.customwidget.custom_view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * create by Mr.Q on 2019/3/2.
+ * 类介绍：
+ *      不可横向滑动的 ViewPager
+ */
+public class NoScrollViewPager extends ViewPager {
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        return false;
+    }
+}
